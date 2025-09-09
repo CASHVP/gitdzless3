6226f33c0fe10980aba419a819026760a9e24503
CASHVP <Trimp19@gmail.com>
2025-09-09 17:56:55 +0300

    ddf

--- src/dz10.java
+++ src/dz10.java
@@ -1,2 +1,20 @@
+import java.util.Scanner;
+
 public class dz10 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Vvedite chisli");
+        int a = sc.nextInt();
+        int d = sc.nextInt();
+        int n = sc.nextInt();
+
+        int c = 0;
+        for (int i = 1; i <= n; i++) {
+            c = a + d * i;
+            System.out.print(" " + c);
+
+        }
+    }
 }
